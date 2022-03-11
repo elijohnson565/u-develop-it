6eff3b4813374b06bb8b3c282c2f5e3c5a2ce2e7
Elijah Johnson <elijohnson565@gmail.com>
2022-03-10 18:09:16 -0800

    added the ability to delete and create candidates in the database

--- server.js
+++ server.js
@@ -28,15 +28,31 @@ app.get('/', (req, res) => {
     });
 });
 
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-    console.log(rows);
+/*
+  // Delete a candidate
+db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log(result);
+  });
+*/
+
+// Create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+VALUES (?,?,?,?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+
+db.query(sql, params, (err, result) => {
+if (err) {
+console.log(err);
+}
+console.log(result);
 });
 
 app.use((req, res) => {
     res.status(404).end();
 });
-
-
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
